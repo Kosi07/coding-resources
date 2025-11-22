daa61b5e4435e57e357c1ed5a8c856a8611a711d
Kosi07 <kosiossai07@gmail.com>
2025-11-22 19:12:04 +0100

    added opengraph meta tags

--- app/html-yt-tutorials/layout.tsx
+++ app/html-yt-tutorials/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import { ReactNode } from 'react'
  
 export const metadata: Metadata = {
-  title: 'html tutorials',
-  description: 'The absolute minimum to start your HTML projects',
+  title: 'HTML Tutorials',
+  description: 'The absolute Minimum required to start your HTML projects',
 }
  
 export default function Layout({children}:{
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,21 @@ import './globals.css';
 import { sfpro } from '@/utils/Fonts';
 
 export const metadata: Metadata = {
-  title: 'coding resources',
+  title: 'Coding Resources',
   description: 'Coding resources for everyone.',
+  openGraph: {
+    title: 'Coding Resources',
+    description: 'Curated list of coding resources for everyone',
+    url: 'https://coding-resources-pond.vercel.app',
+    siteName: 'Coding Resources',
+    images: [
+      {
+        url: 'https://drive.google.com/file/d/1fgD2veW65vAtvtROxM4VCfKgArpWxYBW/view?usp=drivesdk',
+        width: 2672,
+        height: 2090,
+      },
+    ]
+  },
 };
 
 export default function RootLayout({
